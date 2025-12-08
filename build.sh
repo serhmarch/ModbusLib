c62c6682c18a2321bc5626832aab3ee6292bc399
serhmarch <mrchsrg@gmail.com>
2025-12-09 00:34:10 +0200

    Change build.bat, build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/bash
-cmake --preset "Linux-All-Debug"
-cmake --build --preset "Linux-All-Debug"
-
-cmake --preset "Linux-All-Release"
-cmake --build --preset "Linux-All-Release"
-
-cmake --preset "Linux-Qt-No-Examples-Tests-Debug"
-cmake --build --preset "Linux-Qt-No-Examples-Tests-Debug"
+cmake --preset "Linux-No-Examples-Tests-Release"
+cmake --build --preset "Linux-No-Examples-Tests-Release"
 
 cmake --preset "Linux-Qt-No-Examples-Tests-Release"
 cmake --build --preset "Linux-Qt-No-Examples-Tests-Release"
